8d284cd6bb88a711a95d2556f9558b65a0a0737f
amangupta37 <amangupta6121998@gmail.com>
2021-02-12 21:05:36 +0530

    creted a file containing react code

---
+++ Intro-to-React/02-first_react_code/src/index.js
@@ -0,0 +1,22 @@
+// first React app from Scratch
+
+import React from "react"; // import react
+
+import ReactDOM from "react-dom"; //import react dom
+
+/* 
+----------------------  Important Note -----------------------
+
+name of fuction should start with capital letter 
+
+here name of fuction is "App()" which is correct 
+
+but if we name our fuction "app()" then it will generate error
+
+*/
+const App = () =>{   // fucntion containing Html code
+
+ return <h1>Hello World !!!!</h1>;
+
+}
+ReactDOM.render(<App />, document.getElementById("root")); // display output
